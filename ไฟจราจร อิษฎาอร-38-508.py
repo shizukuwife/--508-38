0c4f6f225ce4eb03e1b6c6f7d3b1ab737fe3b895
shizukuwife <wunanimationmeme@gmail.com>
2025-06-30 14:45:20 +0700

    Add files via upload

---
+++ ไฟจราจร อิษฎาอร-38-508.py
@@ -0,0 +1,21 @@
+#logged_in = False
+#if logged_in:
+#    print("Login Success")
+#else:
+#    print("Please try again")
+
+#score = int(input("กรุณากรอกคะแนนของคุณ"))
+#if score >= 50:
+#    print("คุณสอบผ่าน")
+#else :
+#    print("ตกจ้าอีควาย")
+    
+print("ไฟจราจร")
+Color = input("Choose you color(Red , Green , Yellow)")
+if Color == "Red" :
+    print("หยุดรถ")
+elif Color == "Green" :
+    print("ออกรถ")
+elif Color == "Yellow" :
+    print("เตรียมหยุดรถ")
+    
