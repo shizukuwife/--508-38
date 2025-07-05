29a3c6c96b038f3ef313555a0d2600c5a0ec9ee7
shizukuwife <wunanimationmeme@gmail.com>
2025-07-05 23:03:18 +0700

    Add files via upload

---
+++ อิษฎาอร 508 38.py
@@ -0,0 +1,13 @@
+#a = [1, 2, 3,"ahuhihi"]
+#print(a[3])
+
+#a = [1, 2, 3,"shizuai"]
+#a= a[3]
+#print(a)
+
+province = ["Bangkok","กาฬสินธุ์","กาญจนบุรี","จันทบุรี","Peat Riw","Chon buri","พะเยา","Chum porn","ชัยนาท","ขอนแก่น","ชัยภูมิ","ยโสธร","นครพนม","Korat","เชียงใหม่"]
+print(province[0])
+print(province[13])
+print(province[4])
+print(province[5])
+print(province[7])
