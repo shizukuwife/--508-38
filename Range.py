8dedaf0073e422b8af74c011b9fef505d61705c7
shizukuwife <wunanimationmeme@gmail.com>
2025-07-07 14:31:52 +0700

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,25 @@
+#for i in range(1,11):
+#    print (i **2)
+
+#my_data = "shizuku hinomori"
+#for item in my_data:
+#    print(item)
+    
+for i in range(1,101):
+    print(i)
+    print("Ans 100")
+for i in range(7,77,7):
+    print(i)
+    print("Ans 10")
+for i in range(20,1,-2):
+    print(i)
+    print("Ans 10")
+for i in range(2,18,3):
+    print(i)
+    print("Ans 6")
+for i in range(55,0,-11):
+    print(i)
+    print("Ans 5")
+for i in range(1,0):
+    print(i)
+    print("Ans ไม่มีคำตอบ")
